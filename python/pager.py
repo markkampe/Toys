9c44d09ab5ce69b843cf20ad5a12b363e0dfc08a
Mark.Kampe <mark.kampe@gmail.com>
2016-05-23 20:26:55 -0700

    Program to create exam/rubric/answers from std descriptions

---
+++ python/pager.py
@@ -0,0 +1,188 @@
+#
+#
+# this is used by the exam.py utility
+#
+#   a pager filters all of the output that goes into an exm,
+#   doing word-wrapping and page-padding to a specified page size.
+#
+
+
+class pager:
+
+    def __init__(self, outfile, linewid, pagelen):
+        self.output = outfile   # output file
+        self.width = linewid    # line width
+        self.length = pagelen   # page length
+        self.hang = 0           # hanging indent
+
+        self.thisline = ""      # accumulated line
+        self.question = []      # accumulated question
+        self.page = []          # accumulated page
+
+        self.prevBlank = False  # not in blank area
+        self.fill = True        # filling enabled
+        self.drop = 0           # no leading spaces
+
+    # set the hanging indent
+    def setHang(self, hang=0):
+        self.hang = hang
+
+    #
+    # buffer another line to be output at end of page
+    #
+    # (1) handle <PRE></PRE> to disable/enable filling
+    # (2) handle <SPACE> for non-paddable blank lines
+    # (3) every other stretch of blank lines is paddable
+    # (4) if filling is enabled, we fill to line width
+    #
+    def addLine(self, line=None):
+
+        # see if this is a PRE directive
+        stripped = line.strip()
+        if stripped == "<PRE>" or stripped == "<pre>":
+            self.lineBreak()
+            self.fill = False
+            return
+        elif stripped == "</PRE>" or stripped == "</pre>":
+            self.fill = True
+            return
+        elif stripped == "<BR>" or stripped == "<br>":
+            self.lineBreak()
+            return
+        elif stripped == "<SPACE>" or stripped == "<space>":
+            self.lineBreak()
+            self.question.append("\n")
+            self.prevBlank = False
+            return
+
+        # see if this is a padding point
+        isBlank = (len(stripped) == 0) and self.fill
+        if isBlank:
+            self.lineBreak()
+        if isBlank and not self.prevBlank:
+            self.question.append(None)
+        self.prevBlank = isBlank
+
+        # and finally, process the line
+        if self.fill:
+            if isBlank:
+                self.question.append('\n')
+            else:
+                self.lineFill(line)
+        else:
+            self.question.append(line + '\n')
+
+    #
+    # accumulate more text to fill
+    #
+    #   This works pretty well, but handles only a hanging indent.
+    #   It would be cooler if it could infer desired indent from
+    #   the input ... but this only works if the input lines are
+    #   shorter than the output lines.  Otherwise we won't have seen
+    #   the next line by the time we need its indent.
+    #
+    def lineFill(self, line):
+
+        # no whitespace for a new question
+        if len(self.question) == 0 and len(self.thisline) == 0:
+            self.drop = 0
+
+        words = line.split(' ')
+        for w in words:
+            linelen = len(self.thisline)
+            wordlen = len(w)
+            if wordlen == 0:
+                continue
+
+            # have we filled the current line?
+            if (linelen + self.drop + wordlen) > self.width:
+                self.lineBreak()
+
+            # add this word to the line
+            self.thisline += (self.drop * ' ') + w
+
+            # figure out how much space goes after this word
+            #   NOTE: this botches "e.g."
+            if w.endswith(".") or w.endswith("?"):
+                self.drop = 2
+            else:
+                self.drop = 1
+
+    #
+    # force out the accumulated line
+    #
+    def lineBreak(self):
+        if self.thisline:
+            self.question.append(self.thisline + '\n')
+        self.thisline = ""
+        self.drop = self.hang
+
+    #
+    # flush out the accumulated question out to the current
+    # page, forcing out the page if it is full or we are done
+    #
+    def flush(self, force=False):
+        # force out the current line
+        self.lineBreak()
+
+        # see if we need to skip to a new page
+        Plen = len(self.page)
+        Qlen = len(self.question)
+        if Plen + Qlen > self.length:
+            self.force()
+
+        # append this question to the page
+        self.page.extend(self.question)
+        self.question = []
+        self.prevBlank = False
+
+        # see if we need to force it out
+        if force:
+            self.force()
+
+    #
+    # force out the current page
+    #
+    #   A page may include multiple padding points,
+    #   indicated by None lines.  When we flush the
+    #   page out, we will divide any remaining lines
+    #   evenly among the padding points.
+    #
+    def force(self):
+
+        # how many excess lines and padding points
+        padPoints = 0
+        lines = 0
+        for l in self.page:
+            if l is None:
+                padPoints += 1
+            else:
+                lines += 1
+        if padPoints == 0:
+            padPoints = 1
+        excess = self.length - lines
+
+        # force out the buffered questions
+        lines = 0
+        for l in self.page:
+            if l is not None:
+                # normal lines
+                self.output.write(l)
+                lines += 1
+            else:
+                # padding points
+                x = int(excess / padPoints)
+                while x > 0:
+                    self.output.write('\n')
+                    lines += 1
+                    excess -= 1
+                    x -= 1
+                padPoints -= 1
+
+        # pad us out to a page boundary
+        while lines < self.length:
+            self.output.write('\n')
+            lines += 1
+
+        # and reset the buffered page
+        self.page = []
---
+++ python/question.py
@@ -0,0 +1,179 @@
+#
+# this is used by the exam.py utility
+#
+#   The question class understands the format of the question information
+#   files, and knows how to output that information as:
+#       summary
+#       exam question
+#       exam solution
+#       grading rubric
+#
+import sys
+import os.path
+
+
+class question:
+
+    def __init__(self, qname, role=None, number=None, dir=None):
+        """ instantiate a question """
+
+        # see if we can find the file
+        file = dir + "/" + qname if dir else qname
+        if not os.path.exists(file):
+            sys.stderr.write("ERROR: unable to open exam file " + file + "\n")
+            self.input = None
+        else:
+            self.input = open(file, 'rb')
+
+        # default attributes
+        self.name = qname
+        self.number = number
+        self.status = role
+        self.descr = None
+        self.text = None
+        self.lect = None
+        self.difficulty = "  "
+        self.result = None
+        self.priority = 0
+        self.lines = 1
+        self.time = 0
+        self.head = "   #  sts Q-ID  P,DN Description                Reading         Lecture"
+        self.dash = "  --  --- ----  ---- ------------               -------         -------"
+        self.format = "%3s %-5s %d,%s %-23.23s\t%-14.14s\t%s"
+
+
+    def close(self):
+        self.input.close()
+
+    def heading(self, lines=False):
+        return self.dash if lines else self.head
+
+    def summary(self, output=None):
+        """ read the summary and return the description """
+
+        if self.input is None:
+            return "ERROR"
+
+        # process attribute=value lines until we hit the question
+        self.input.seek(0)
+        for line in self.input:
+            if "===QUESTION===" in line:
+                prefix = ("  %2s: " %
+                         (self.number)) if self.number else "      "
+                args = (self.status, self.name, self.priority, self.difficulty,
+                        self.descr, self.text, self.lect)
+                return prefix + (self.format % args)
+            else:
+                name, var = line.partition("=")[::2]
+                if name == "descr":
+                    self.descr = var.rstrip()
+                elif name == "text" and var.rstrip() != "":
+                    self.text = var.rstrip()
+                elif name == "lect" and var.rstrip() != "":
+                    self.lect = var.rstrip()
+                elif name == "status" and var.rstrip() != "":
+                    self.status = var.rstrip()
+                elif name == "diff" and var.rstrip() != "":
+                    self.difficulty = var.rstrip()
+                elif name == "lines" and var.rstrip() != "":
+                    self.lines = int(var)
+                elif name == "pri" and var.rstrip() != "":
+                    self.priority = int(var)
+                elif name == "time" and var.rstrip() != "":
+                    self.time = int(var)
+                elif name == "result" and var.rstrip() != "":
+                    self.result = var.rstrip()
+
+    def printExam(self, pager):
+        """ print out the exam question """
+        if self.input is None:
+            return
+
+        # set the hanging indent
+        pager.setHang(len(self.number) + 2)
+
+        # seek to the question
+        inSection = False
+        firstLine = False
+        self.input.seek(0)
+        for line in self.input:
+            if "===ANSWER===" in line:
+                break
+            elif "===QUESTION===" in line:
+                inSection = True
+                firstLine = True
+            elif inSection:
+                if firstLine:
+                    prefix = "%s: " % (self.number)
+                    pager.addLine(prefix + line.rstrip())
+                    firstLine = False
+                else:
+                    pager.addLine(line.rstrip())
+
+        # see if we owe any padding lines
+        l = self.lines
+        while l > 0:
+            pager.addLine('\n')
+            l -= 1
+        pager.flush()
+
+    def printSolution(self, output):
+        """ print out problem solution  """
+        if self.input is None:
+            return
+
+        # seek to the question
+        inSection = False
+        firstLine = False
+        self.input.seek(0)
+        for line in self.input:
+            if "===RUBRIC===" in line:
+                output.write('\n')
+                break
+            elif "===ANSWER===" in line:
+                inSection = True
+                firstLine = True
+            elif inSection:
+                if firstLine:
+                    self.solnIntro(output)
+                    firstLine = False
+                output.write("    " + line)
+
+    def printRubric(self, output):
+        """ print out problem rubric  """
+        if self.input is None:
+            return
+
+        # seek to the question
+        inSection = False
+        firstLine = False
+        self.input.seek(0)
+        for line in self.input:
+            if "===NOTES===" in line:
+                output.write('\n')
+                break
+            elif "===RUBRIC===" in line:
+                inSection = True
+                firstLine = True
+            elif inSection:
+                if firstLine:
+                    title = "%s(%s): %s\n" % \
+                            (self.number, self.name, self.descr)
+                    output.write(title)
+                    firstLine = False
+                output.write("\t" + line)
+
+    def solnIntro(self, output):
+        """ print out the start of a solution """
+
+        title = "<H2>%s. %s</H2>\n" % (self.number, self.descr)
+        output.write(title)
+
+        if self.text or self.lect:
+            refmsg = "This was discussed in %s section(s) %s<br>\n"
+            output.write("<P>\n")
+            if self.text:
+                output.write(refmsg % ("reading", self.text))
+            if self.lect:
+                output.write(refmsg % ("lecture", self.lect))
+            output.write("</P>\n")
