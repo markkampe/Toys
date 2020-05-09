399f577e3f3da19519261ad448a2c48949f77534
Mark.Kampe <mark.kampe@gmail.com>
2020-05-09 09:47:53 -0700

    search can now discover multiple objects

--- card_play/gamecontext.py
+++ card_play/gamecontext.py
@@ -10,12 +10,6 @@ class GameContext(GameObject):
     higherarchical relationships (e.g. kingdom, village, buiding, room).
     """
 
-    # skills that might be applicable in any context
-    # TODO search no longer has a mapped save
-    skill_map = {
-        "SEARCH": "perception"
-        }
-
     def __init__(self, name, descr=None, parent=None):
         """
         create a new GameObject
@@ -71,22 +65,6 @@ class GameContext(GameObject):
         """
         # get the list of actions for this context
         actions = super(GameContext, self).possible_actions(actor, context)
-
-        # if this verb has a skill associated with it, add it to the action
-        for action in actions:
-            # does the character have this as an explicit skill
-            skill = actor.get(action.verb)
-            if skill is not None:
-                action.set("skill", skill)
-                continue
-
-            # is this action a function of an attribute
-            if action.verb in self.skill_map.keys():
-                attribute = self.skill_map[action.verb]
-                value = actor.get(attribute)
-                if value is not None:
-                    action.set("skill", value)
-
         return actions
 
     def accept_action(self, action, actor, context):
@@ -100,28 +78,14 @@ class GameContext(GameObject):
         """
 
         # A locale can be searched, turning up concealed things.
-        # a Search action will have a "skill" attribute, indicating
-        # the searcher's skills at searching.
         if action.verb == "SEARCH":
-            ability = action.get("skill")
-            if ability is None:
-                ability = 0
-            found = None
+            result = ""
             for thing in self.objects:
-                concealment = thing.get("concealment")
+                concealment = thing.get("RESISTANCE.SEARCH")
                 if concealment is not None and concealment > 0:
-                    roll = randint(1, 100)
-                    if roll + ability > concealment:
-                        if found is None:
-                            found = thing.name
-                        else:
-                            found += ", " + thing.name
-                        thing.set("concealment", 0)
-
-            if found is None:
-                result = "You found nothing"
-            else:
-                result = "You found " + found
+                    if result != "":
+                        result += "\n    "
+                    result += thing.accept_action(action, actor, context)
         else:
             # if we don't recognize this action, pass it up the chain
             result = super(GameContext, self).accept_action(action,
