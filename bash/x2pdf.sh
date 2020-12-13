30011a8ed851d78ed85a84690b15bca6d68276c9
Mark.Kampe <mark.kampe@gmail.com>
2020-12-13 09:59:42 -0800

    script for turning txt->pdf for Gradescope

---
+++ bash/x2pdf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Turn an ASCII text exam submission into a pdf for up-load to Gradescope.
+#
+# input file should one question per page, ASCII text exam submission
+#    likely obtained by downloading from Sakai Assignments
+#
+# usage: x2pdf input_file output_basename
+#	e.g. x2pdf x1.txt markk
+#	create markk.pdf from x1.txt
+#
+if [ ! -r $1 ]
+then
+	echo "ERROR: input file $1 does not exist" >& 2
+	exit 1
+fi
+
+if [ -z "$2" ]
+then
+	echo "usage: $0 input-file student-name" >& 2
+	exit 1
+fi
+
+echo " ... Converting $1 to $2.ps"
+enscript -B -L70 --margins=::0:0 -o $2.ps $1 2>stderr
+if [ $? -eq 0 ]
+then
+	sed -e "s/^/     /" < stderr
+	echo " ... Converting $2.ps to $2.pdf"
+	ps2pdf $2.ps $2.pdf
+	if [ $? -eq 0 ]
+	then
+		rm -f stderr $2.ps
+		exit 0
+	else
+		cat stderr
+		exit 1
+	fi
+else
+	exit 1
+fi
